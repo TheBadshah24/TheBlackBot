bd2fa3bcda128c9d8819a39d1e45e32d080496f9
TheBlackXYZ <155623566+TheBlackxyz@users.noreply.github.com>
2024-07-16 18:24:47 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TheBlackxyz/TheBlackBot.git /Auto-search-tamil-bot
+  git clone https://github.com/TheBlackxyz/TheBlackBot.git /TheBlackBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Auto-search-tamil-bot
+  git clone $UPSTREAM_REPO /TheBlackBot
 fi
 cd /TheBlackBot
 pip3 install -U -r requirements.txt
