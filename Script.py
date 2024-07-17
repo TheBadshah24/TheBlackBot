aca2fa79dffdd7d46671eb7dac46c423ad0082cb
TheBlackXYZ <155623566+TheBlackxyz@users.noreply.github.com>
2024-07-17 10:31:38 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -230,7 +230,13 @@ Wᴇʟᴄᴏᴍᴇ ᴛᴏ {chat}.
 👀 DATA Pass From - @TheBlackXYZ """
    
 
+RESTART_TXT = """
+<b>Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ !
 
+📅 Dᴀᴛᴇ : <code>{}</code>
+⏰ Tɪᴍᴇ : <code>{}</code>
+🌐 Tɪᴍᴇᴢᴏɴᴇ : <code>Asia/Kolkata</code>
+🛠️ Bᴜɪʟᴅ Sᴛᴀᴛᴜs: <code>v2.7.1 [ Sᴛᴀʙʟᴇ ]</code></b>"""
 
    
   
