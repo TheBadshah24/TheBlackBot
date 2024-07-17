ef36793bfa1b0f96ee315543c3359c1465ea79c1
TheBlackXYZ <155623566+TheBlackxyz@users.noreply.github.com>
2024-07-17 09:25:15 +0530

    Update openai.py

--- plugins/ExtraMods/openai.py
+++ plugins/ExtraMods/openai.py
@@ -6,14 +6,14 @@
 # For Any ERROR Please Contact Me -> Telegram ->@TheBlackXYZBotz & Insta @TheBlackXYZ
 # Please Love & Support 💗💗🙏
 
-from pyrogram import Client, filters
-from plugins.ExtraMods.engine import ask_ai
+#from pyrogram import Client, filters
+#from plugins.ExtraMods.engine import ask_ai
 
 
-@Client.on_message(filters.command('openai'))
-async def openai_ask(client, message):
-    if len(message.command) == 1:
-       return await message.reply_text("Give an input!")
-    m = await message.reply_text("👀")
-    await ask_ai(client, m, message)
+#@Client.on_message(filters.command('openai'))
+#async def openai_ask(client, message):
+ #   if len(message.command) == 1:
+  #     return await message.reply_text("Give an input!")
+ #   m = await message.reply_text("👀")
+  #  await ask_ai(client, m, message)
   
