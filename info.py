1a955161844def25adfcfae39c841e955ff97135
TheBlackXYZ <155623566+TheBlackxyz@users.noreply.github.com>
2024-07-18 09:53:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,11 +3,13 @@ from os import environ
 from Script import script 
 
 id_pattern = re.compile(r'^.\d+$')
-
 def is_enabled(value, default):
-    if value.strip().lower() in ["on", "true", "yes", "1", "enable", "y"]: return True
-    elif value.strip().lower() in ["off", "false", "no", "0", "disable", "n"]: return False
-    else: return default
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
 
 
 # PyroClient Setup 
