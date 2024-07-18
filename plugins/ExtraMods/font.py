891bd7d9fd8289b3b111358de00c3615767914cb
TheBlackXYZ <155623566+TheBlackxyz@users.noreply.github.com>
2024-07-18 07:09:57 +0530

    Update and rename plugins/font.py to plugins/ExtraMods/font.py

--- plugins/ExtraMods/font.py
+++ plugins/ExtraMods/font.py
@@ -1,5 +1,5 @@
 import os
-from plugins.ExtraMods.fotnt_string import Fonts
+from Font.fotnt_string import Fonts
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
