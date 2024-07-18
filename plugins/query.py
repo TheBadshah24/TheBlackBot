758fda47a8d5d5fd3cf28fa3d5f9a9aa97efd3e3
TheBlackXYZ <155623566+TheBlackxyz@users.noreply.github.com>
2024-07-18 14:26:05 +0530

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -16,7 +16,7 @@ from database.ia_filterdb import Media, get_file_details, get_search_results
 from database.filters_mdb import del_all, find_filter, get_filters
 from database.gfilters_mdb import find_gfilter, get_gfilters
 from database.users_chats_db import db
-from info import *
+#from info import *
 
 # Configuration
 from info import ADMINS, AUTH_CHANNEL, AUTH_USERS, CUSTOM_FILE_CAPTION, AUTH_GROUPS, P_TTI_SHOW_OFF, PICS, IMDB, PM_IMDB, SINGLE_BUTTON, PROTECT_CONTENT, SPELL_CHECK_REPLY, IMDB_TEMPLATE, IMDB_DELET_TIME, START_MESSAGE, PMFILTER, G_FILTER, BUTTON_LOCK, BUTTON_LOCK_TEXT, SHORT_URL, SHORT_API
