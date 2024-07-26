526ec62e8dbe3ef8d76ca72a4579331e92c0e9f2
TheBlackXYZ <155623566+TheBlackxyz@users.noreply.github.com>
2024-07-26 20:36:34 +0530

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -297,20 +297,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
         except Exception as e:
             await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
      
-   elif query.data.startswith("checksub"):
-        if AUTH_CHANNEL and not await is_subscribed(client, query):
-            return await query.answer("I Lɪᴋᴇ Yᴏᴜʀ Sᴍᴀʀᴛɴᴇss, Bᴜᴛ Dᴏɴ'ᴛ Bᴇ Oᴠᴇʀsᴍᴀʀᴛ Oᴋᴀʏ 😏", show_alert=True)
-        ident, file_id = query.data.split("#")
-        files_ = await get_file_details(file_id)
-        if not files_: return await query.answer('NO SUCH FILE EXIST....')
-        files = files_[0]
-        title = files.file_name
-        size = get_size(files.file_size)
-        f_caption = f_caption = f"{title}"
-        if CUSTOM_FILE_CAPTION:
-            try: f_caption = CUSTOM_FILE_CAPTION.format(mention=query.from_user.mention, file_name='' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)  
-            except Exception as e: logger.exception(e)
-        await client.send_cached_media(chat_id=query.from_user.id, file_id=file_id, caption=f_caption, protect_content=True if ident == 'checksubp' else False)
         
         elif query.data == "start":                        
         buttons = [[ 
